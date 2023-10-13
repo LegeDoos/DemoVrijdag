9ed98ce408000bc627c912e4355e9c44314d082a
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2023-10-13 16:16:06 +0200

    Add database context to program.cs DI of API project Add connection string to appsettings of API project Create API controller in MVC project and move to API project Cleanup

---
+++ DemoVrijdagAPI/DemoVrijdagAPI/Controllers/CoursesAPIController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DemoVrijdag.Models;
+
+namespace DemoVrijdag.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesAPIController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+
+        public CoursesAPIController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CoursesAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Course>>> GetCourse()
+        {
+          if (_context.Course == null)
+          {
+              return NotFound();
+          }
+            return await _context.Course.ToListAsync();
+        }
+
+        // GET: api/CoursesAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Course>> GetCourse(int id)
+        {
+          if (_context.Course == null)
+          {
+              return NotFound();
+          }
+            var course = await _context.Course.FindAsync(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return course;
+        }
+
+        // PUT: api/CoursesAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCourse(int id, Course course)
+        {
+            if (id != course.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(course).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CoursesAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Course>> PostCourse(Course course)
+        {
+          if (_context.Course == null)
+          {
+              return Problem("Entity set 'AppDBContext.Course'  is null.");
+          }
+            _context.Course.Add(course);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCourse", new { id = course.Id }, course);
+        }
+
+        // DELETE: api/CoursesAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourse(int id)
+        {
+            if (_context.Course == null)
+            {
+                return NotFound();
+            }
+            var course = await _context.Course.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            _context.Course.Remove(course);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CourseExists(int id)
+        {
+            return (_context.Course?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- DemoVrijdagAPI/DemoVrijdagAPI/Program.cs
+++ DemoVrijdagAPI/DemoVrijdagAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using DemoVrijdag.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace DemoVrijdagAPI
 {
     public class Program
@@ -13,6 +16,12 @@ namespace DemoVrijdagAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDBContext>(
+            DbContextOptions =>
+                DbContextOptions.UseSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
             var app = builder.Build();
 
