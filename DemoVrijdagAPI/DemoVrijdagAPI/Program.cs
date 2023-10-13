6051518373e0b97c9f9ee540890982b128b21496
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2023-10-13 16:25:40 +0200

    Toevoegen enfo aan swagger

--- DemoVrijdagAPI/DemoVrijdagAPI/Program.cs
+++ DemoVrijdagAPI/DemoVrijdagAPI/Program.cs
@@ -1,6 +1,8 @@
 
 using DemoVrijdag.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace DemoVrijdagAPI
 {
@@ -22,6 +24,30 @@ namespace DemoVrijdagAPI
                 DbContextOptions.UseSqlServer(
                 builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "DemoVrijdag API",
+                    Description = "Een erg mooie API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Meer info",
+                        Url = new Uri("https://github.com/LegeDoos/DemoVrijdag")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Deze licentie is van toepassing",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+
+                // using System.Reflection;
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
 
             var app = builder.Build();
 
